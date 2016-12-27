20fd4f9665b4e8290fefb05ad0b2b20311d56598
Sébastien Roccaserra <sroccaserra@yahoo.com>
2016-12-27 18:27:06 +0100

    added a Console class

--- feature/test_print_statement_feature.py
+++ feature/test_print_statement_feature.py
@@ -1,7 +1,15 @@
 from unittest.mock import Mock
+from src.console import Console
 
 def test_print_statement_containing_all_transactions():
-    console = Mock(["print_line"])
+    # Given
+    console = Mock(Console)
+    account = Mock(["print_statement"])
+
+    # When
+    account.print_statement()
+
+    # Then
     console.print_line.assert_called_with("DATE | AMOUNT | BALANCE")
     console.print_line.assert_called_with("10/04/2014 | 500.00 | 1400.00")
     console.print_line.assert_called_with("02/04/2014 | -100.00 | 900.00")
