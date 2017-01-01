cf3f4f4e490087de3b79d2a37b983402b1f4e246
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-01-01 14:19:13 +0100

    inherited abstract classes from ABC

--- app/domain/statement_printer.py
+++ app/domain/statement_printer.py
@@ -1,3 +1,7 @@
-class StatementPrinter:
+from abc import ABC, abstractmethod
+
+
+class StatementPrinter(ABC):
+    @abstractmethod
     def print(self, transactions):
         raise NotImplementedError()
--- app/domain/transaction_repository.py
+++ app/domain/transaction_repository.py
@@ -1,9 +1,15 @@
-class TransactionRepository:
+from abc import ABC, abstractmethod
+
+
+class TransactionRepository(ABC):
+    @abstractmethod
     def add_deposit(self, amount):
         raise NotImplementedError()
 
+    @abstractmethod
     def add_withdrawal(self, amount):
         raise NotImplementedError()
 
+    @abstractmethod
     def all_transactions(self):
         raise NotImplementedError()
